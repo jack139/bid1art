6478ecbc4796319f936b759ea806f3ca5d116e09
gt <jack139@gmail.com>
2021-05-16 16:46:07 +0800

    增加拍卖申请

---
+++ src/user/auc.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+from chain_api import fork_api
+
+# 拍卖列表
+
+url = ('/auc/list')
+
+PAGE_SIZE = 8
+
+class handler:
+    def GET(self):
+        if not helper.logged(helper.PRIV_TRD|helper.PRIV_DEL|helper.PRIV_ART, 'AUC'):
+            raise web.seeother('/')
+
+        user_data=web.input(page='1', owner='')
+        render = helper.create_render()
+
+        if not user_data['page'].isdigit():
+            return render.info('page参数错误！')  
+
+        # 链上数据
+        r1 = fork_api('/query/auction/list', {
+            'page'  : int(user_data['page']),
+            'limit' : PAGE_SIZE,
+            'owner_addr' : user_data['owner'],
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请稍后再试！(%s %s)'%((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        items=[]
+        for u in r1['data']['auction_list']:
+            items.append([u['id'],u['itemId'],u['auctionHouseId'],u['reqDate'],])
+
+        return render.auc(helper.get_session_uname(), helper.get_privilege_name(), helper.get_session_addr(),
+            items, int(user_data['page']), len(items)==PAGE_SIZE, len(user_data['owner']))
+
---
+++ src/user/auc_add.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+from chain_api import fork_api
+
+# 新建拍卖请求
+
+url = ('/auc/add')
+
+
+class handler:
+    def GET(self):
+        if not helper.logged(helper.PRIV_TRD|helper.PRIV_DEL|helper.PRIV_ART, 'AUC'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        return render.auc_new(helper.get_session_uname(), helper.get_privilege_name(), helper.get_session_addr())
+
+
+    def POST(self):
+        if not helper.logged(helper.PRIV_TRD|helper.PRIV_DEL|helper.PRIV_ART, 'AUC'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data=web.input(owner_addr='', desc='', 
+            detail='', date='', type='', subject='', media='', size='', base_price='')
+        print(user_data)
+
+        if '' in [user_data.owner_addr, user_data.desc, user_data.date]:
+            return render.info('物品名称、所有者链地址、年代均不能为空！')  
+
+        # 链上新建用户
+        r1 = fork_api('/biz/auction/new', {
+            'owner_addr'   : user_data['owner_addr'],
+            'desc'         : user_data['desc'],
+            'detail'       : user_data['detail'],
+            'date'         : user_data['date'],
+            'type'         : user_data['type'],
+            'subject'      : user_data['subject'],
+            'media'        : user_data['media'],
+            'size'         : user_data['size'],
+            'base_price'   : user_data['base_price'],
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请稍后再试！(%s %s)'%((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        return render.info('提交成功！','/auc/list')
