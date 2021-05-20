fbc40ead91dd8c2f7837062d84f7af37adf8c6ea
gt <jack139@gmail.com>
2021-05-20 17:58:15 +0800

    auc_info

---
+++ src/user/auc_info.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+from chain_api import fork_api
+
+# 物品信息
+
+url = ('/auc/info')
+
+class handler:
+    def GET(self):
+        if not helper.logged(helper.PRIV_TRD|helper.PRIV_DEL|helper.PRIV_ART, 'AUC'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data=web.input(auc_id='')
+
+        if user_data.auc_id=='':
+            return render.info('错误的参数！')  
+
+        # 获取拍卖信息
+        r1 = fork_api('/query/auction/info', {
+            'id' : user_data.auc_id,
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请联系管理员！(%s %s)'%\
+                ((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        # 获取艺术品信息
+        r2 = fork_api('/query/item/info', {
+            'id' : r1['data']['auction']['item_id'],
+        })
+        if (r2 is None) or r2['code']!=0:
+            return render.info('出错了，请联系管理员！(%s %s)'%\
+                ((r2['code'], r2['msg']) if r2 else ('', '')))
+
+        return render.auc_info(helper.get_session_uname(), helper.get_privilege_name(), helper.get_session_addr(),
+            r1['data']['auction'], r2['data']['item'])
