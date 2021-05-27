89f1ae371c1fe1f08b0d8c4fcb6ad726b539b334
gt <jack139@gmail.com>
2021-05-27 10:31:35 +0800

    审核拍卖申请

---
+++ src/super/auc_audit.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+from chain_api import fork_api
+
+# 拍卖申请审核
+
+url = ('/plat/auc_audit')
+
+
+class handler:
+    def GET(self):
+        if not helper.logged(helper.PRIV_AH, 'AUC_OP'):
+            raise web.seeother('/')
+
+        user_data=web.input(page='1')
+        render = helper.create_render()
+
+        if not user_data['page'].isdigit():
+            return render.info('page参数错误！')  
+
+        # 链上数据
+        r1 = fork_api('/query/auction/list_by_status', {
+            'page'   : int(user_data['page']),
+            'limit'  : setting.PAGE_SIZE,
+            'status' : 'WAIT'
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请稍后再试！(%s %s)'%((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        return render.auc_audit(helper.get_session_uname(), helper.get_privilege_name(), helper.get_session_addr(),
+            r1['data']['auction_list'], int(user_data['page']), len(r1['data']['auction_list'])==setting.PAGE_SIZE)
---
+++ src/super/auc_audit_edit.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+from chain_api import fork_api
+
+# 审核拍卖申请
+
+url = ('/plat/auc_audit_edit')
+
+
+class handler:
+    def GET(self):
+        if not helper.logged(helper.PRIV_AH, 'AUC_OP'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data=web.input(auc_id='')
+
+        if user_data.auc_id=='':
+            return render.info('错误的参数！')  
+
+        # 获取用户信息
+        r1 = fork_api('/query/auction/info', {
+            'id' : user_data.auc_id,
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请联系管理员！(%s %s)'%\
+                ((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        # 获取艺术品信息
+        r2 = fork_api('/query/item/info', {
+            'id' : r1['data']['auction']['item_id'],
+        })
+        if (r2 is None) or r2['code']!=0:
+            return render.info('出错了，请联系管理员！(%s %s)'%\
+                ((r2['code'], r2['msg']) if r2 else ('', '')))
+
+        status_list = [
+            ('WAIT', '待审核'),
+            ('INIT', '审核通过'),
+            ('NOGO', '审核不通过'),
+            ('LOCK', '锁定'),
+        ]
+
+        return render.auc_audit_edit(helper.get_session_uname(), helper.get_privilege_name(), helper.get_session_addr(),
+            r1['data']['auction'], r2['data']['item'], status_list)
+
+
+    def POST(self):
+        if not helper.logged(helper.PRIV_AH, 'AUC_OP'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data=web.input(auc_id='', open_date='', close_date='', status='')
+
+        if '' in (user_data.auc_id, user_data.open_date, user_data.close_date, user_data.status):
+            return render.info('参数错误！')  
+
+        # 链上修改用户信息
+        r1 = fork_api('/biz/audit/auction', {
+            'caller_addr': helper.get_session_addr(),
+            'id'         : user_data['auc_id'],
+            'status'     : user_data['status'],
+            'open_date'  : user_data['open_date'],
+            'close_date' : user_data['close_date'],
+        })
+        if (r1 is None) or r1['code']!=0:
+            return render.info('出错了，请稍后再试！(%s %s)'%((r1['code'], r1['msg']) if r1 else ('', '')))
+
+        return render.info('成功保存！','/plat/auc_audit')
